5fc81487c15d8c96ef2906e2e9871bd838b0070d
haazkil <sayadan9@gmail.com>
2022-01-09 03:24:57 +0700

    first commit

---
+++ js/jss.js
@@ -0,0 +1,23 @@
+// skilss
+
+const skillsContent = document.getElementsByClassName('skills_content'),
+  skillsHeader = document.querySelectorAll('.skills_header');
+
+function toggleSkills() {
+  let itemClass = this.parentNode.className
+
+  for (i = 0; i < skillsContent.length; i++) {
+    skillsContent[i].className = 'skills_content skills_close';
+  }
+  if (itemClass === 'skills_content skills_close') {
+    this.parentNode.className = 'skills_content skills_open';
+  }
+}
+
+skillsHeader.forEach((el) =>{
+  el.addEventListener('click', toggleSkills)
+}) 
+
+$(document).on('click', 'ul li', function(){
+  $(this).addClass('active').siblings().removeClass('active')
+})
